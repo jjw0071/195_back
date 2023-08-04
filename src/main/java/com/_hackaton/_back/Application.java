d58795e771d01b189cbea31601c239b555136607
jjw0071 <jjw0071@naver.com>
2023-08-04 12:45:37 +0900

    Initial commit

---
+++ src/main/java/com/_hackaton/_back/Application.java
@@ -0,0 +1,13 @@
+package com._hackaton._back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
