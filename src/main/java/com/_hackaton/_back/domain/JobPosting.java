4fb00a12d53de5bc936881efe60ccce16f096abb
jjw0071 <jjw0071@naver.com>
2023-08-07 16:39:49 +0900

    Refactor: Update field names in JobPosting entity to follow Java naming conventions

--- src/main/java/com/_hackaton/_back/domain/JobPosting.java
+++ src/main/java/com/_hackaton/_back/domain/JobPosting.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,19 +16,19 @@ public class JobPosting {
     @Id
     @GeneratedValue
     private Long id;
-    private String company_name; //회사이름
-    private String phone_number; //전화번호
-    private String profile_image; //기업이미지
+    private String companyName; //회사이름
+    private String phoneNumber; //전화번호
+    private String profileImage; //기업이미지
     private String sector; //
     private String region;
-    private String company_images;
-    private String detail_address;
-    private String employment_period;
-    private String workdays;
-    private String workhour;
+    private List<String> companyImages;
+    private String detailAddress;
+    private String employmentPeriod;
+    private String workDays;
+    private String workHours;
     private String details;
     private int salary;
-    private boolean provide_accommodation;
+    private boolean provideAccommodation;
 
 
 }
